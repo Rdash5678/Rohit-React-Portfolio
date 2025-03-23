03f15132eaa8ff773d5dc03bd54db2cd4ff4bba1
RohitDashaputra <67654566+Rdash5678@users.noreply.github.com>
2025-03-23 16:54:14 -0500

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -51,19 +51,19 @@ export const navLinks: TNavLink[] = [
 
 const services: TService[] = [
   {
-    title: "Web Developer",
+    title: "Hardware-Software Integrator",
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "STEM Education Advocate",
     icon: mobile,
   },
   {
-    title: "Backend Developer",
+    title: "Systems Architect",
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Firmware Developer",
     icon: creator,
   },
 ];
@@ -125,29 +125,30 @@ const technologies: TTechnology[] = [
 
 const experiences: TExperience[] = [
   {
-    title: "React.js Developer",
-    companyName: "Starbucks",
+    title: "Computer Science Consulting & Curriculum Devolpment Intern",
+    companyName: "Xperience Coding LLC",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "March 2024 - July 2024",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Developed and tailored comprehensive STEM camp curricula for 2nd-8th graders, incorporating core principles of computer science, programming, and engineering. Projects included robotics with Battle Bots, CAD modeling for 3D-printed solutions, and introductory Python and JavaScript coding for hands-on application.",
+      "Led curriculum consultations, leveraging knowledge of computer engineering to introduce hardware concepts such as microcontrollers and circuits, fostering a deeper understanding of how software interacts with physical systems.",
+      "Mentored and trained high school volunteers, teaching them to integrate coding into engineering projects, providing real-world teaching experience while aligning with principles of embedded systems and hardware/software integration.",
+      "Spearheaded the creation of interactive, scalable coding lessons by designing projects with a balance of theoretical and practical exercises, ensuring students understood foundational engineering concepts like data structures, algorithms, and the hardware-software relationship.",
+      "Optimized lesson plans by incorporating computer engineering concepts such as embedded systems, microprocessor basics, and sensor integration, enhancing students' exposure to interdisciplinary applications of coding.",
     ],
   },
   {
-    title: "React Native Developer",
-    companyName: "Tesla",
+    title: "Co - President of Education and Buisness",
+    companyName: "Xperience Coding LLC",
     icon: tesla,
     iconBg: "#E6DEDD",
     date: "Jan 2021 - Feb 2022",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      "Transformed Xperience Coding LLC into a thriving 501(c)(3) nonprofit, leveraging leadership and vision to expand the organization beyond a school-wide event into a community-driven initiative that now serves hundreds of students, offering free coding and engineering camps to youth in the greater Lake County area.",
+      "Developed and implemented dynamic STEM curricula that introduced students to coding, robotics, and engineering principles, ensuring a blend of hands-on learning with cutting-edge technologies, such as IoT, embedded systems, and 3D printing, while continually adapting the curriculum to meet the evolving needs of the community.",
+      "Led a diverse team of high school volunteers, inspiring them to take ownership of lessons and projects, providing them with mentorship and leadership opportunities to grow as educators, and ensuring the smooth delivery of programming across multiple locations and age groups.",
+      "Expanded the nonprofit’s reach, coordinating efforts to partner with local businesses and organizations, leading to the creation of innovative tech projects, such as virtual tours and community outreach events, and driving the nonprofit’s mission forward with increased visibility and impact.",
     ],
   },
   {
@@ -207,58 +208,54 @@ const testimonials: TTestimonial[] = [
 
 const projects: TProject[] = [
   {
-    name: "Car Rent",
+    name: "Algebr.ai",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      "Algebr.ai is an AI-powered platform that helps students master Algebra I and II with customizable practice tests, real-time difficulty adjustments, and performance tracking. Built with modern web technologies, it offers an engaging learning experience.",
     tags: [
       {
-        name: "react",
+        name: "Python",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
+        name: "Django",
         color: "green-text-gradient",
       },
       {
-        name: "tailwind",
+        name: "PostgreSQL",
         color: "pink-text-gradient",
       },
     ],
     image: carrent,
-    sourceCodeLink: "https://github.com/",
+    sourceCodeLink: "https://github.com/okotwal17/Algebr.ai",
   },
   {
-    name: "Job IT",
+    name: "Free-Scribe",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      "Free Scribe is a machine learning app that converts voice to text in real time. Built with React and TailwindCSS, it offers an adaptive, mobile-first design, optimized for accuracy and performance. The app integrates speech-to-text functionality, providing a seamless experience across devices and environments, making it ideal for various use cases.",
     tags: [
       {
         name: "react",
         color: "blue-text-gradient",
       },
       {
-        name: "restapi",
+        name: "TailwindCSS",
         color: "green-text-gradient",
-      },
-      {
-        name: "scss",
-        color: "pink-text-gradient",
       },
     ],
     image: jobit,
-    sourceCodeLink: "https://github.com/",
+    sourceCodeLink: "https://github.com/Rdash5678/MachineLearningAPP",
   },
   {
-    name: "Trip Guide",
+    name: "Nasa APP",
     description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      "NASA React App is a dynamic web application built with React that integrates NASA's API to display real-time data such as images, news, and scientific information. I utilized React hooks for efficient state management and API calls, ensuring a seamless user experience. Custom CSS was used for professional, responsive design. This project enhanced my skills in API integration, front-end development, and creating scalable, maintainable React components.",
     tags: [
       {
-        name: "nextjs",
+        name: "API-integration",
         color: "blue-text-gradient",
       },
       {
-        name: "supabase",
+        name: "React",
         color: "green-text-gradient",
       },
       {
